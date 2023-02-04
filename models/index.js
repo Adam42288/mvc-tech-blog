91d2fd5f3a080b23006335be78231e6078df1320
Adam <asantillana@apple.com>
2023-02-04 17:37:13 -0600

    added the rest of my js for index.js model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,33 @@
 // Create an association between the models and import them
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
 // Create the model associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
