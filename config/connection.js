566ab24f513ac335ac986234b18127c8ccf4cf9a
Adam <asantillana@apple.com>
2023-02-06 15:20:56 -0600

    updated connection.js with sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,28 @@
 // Import sequelize
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 // Require dotenv to use .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Initialize a sequelize instance to connect to our MySQL DB.
-let sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    });
-}
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     });
+// }
 
 module.exports = sequelize;
