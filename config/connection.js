61c0a0ccfbe49a5adcd3d233bd080d61129cc92d
Adam <asantillana@apple.com>
2023-02-06 15:47:55 -0600

    updated connection.js with JAWSDB support

--- config/connection.js
+++ config/connection.js
@@ -4,25 +4,20 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // Initialize a sequelize instance to connect to our MySQL DB.
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
 
-// if (process.env.JAWSDB_URL) {
-//     sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//         host: 'localhost',
-//         dialect: 'mysql',
-//         port: 3306
-//     });
-// }
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
